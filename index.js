f2ea7632c52a3f62071530566e55c710b707a31f
Mohammed Sadik <msadikjowel@gmail.com>
2022-07-26 21:19:28 +0600

    counter app

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-// define constant
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
+// state - count: 0
+// action - increment, decrement, reset
+// reducer
+// store
 
-// state
+const { createStore } = require("redux");
+
+// Constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
 const initialState = {
-    count: 0,
+    count: 0
 };
 
-// action
-const incrementCounter = () => {
+// Actions
+const incrementAction = () => {
     return {
         type: INCREMENT,
     }
 };
-const decrementCounter = () => {
+
+const decrementAction = () => {
     return {
         type: DECREMENT,
     }
 };
 
+const resetAction = () => {
+    return {
+        type: RESET,
+    }
+};
 
-// reducer
+// creating reducer
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
             return {
                 ...state,
-                count: state.count + 1
+                count: state.count + 1,
             };
+
         case DECREMENT:
             return {
                 ...state,
-                count: state.count - 1
+                count: state.count - 1,
             };
+
+        case RESET:
+            return {
+                ...state,
+                count: 0,
+            };
+
         default:
             state;
     }
 };
+
+// Store
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(incrementAction())
+store.dispatch(incrementAction())
+store.dispatch(incrementAction())
+store.dispatch(incrementAction())
+store.dispatch(incrementAction())
+store.dispatch(decrementAction())
+store.dispatch(resetAction())
